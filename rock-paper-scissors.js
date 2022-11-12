1282b39c1e59341837a0052641dae6cca76803c9
Augelvin Theone Dermaga <augelvintd@gmail.com>
2022-11-12 15:30:22 -0800

    add function to generate random computer choice

---
+++ rock-paper-scissors.js
@@ -0,0 +1,25 @@
+/* function to get random computer choice */
+function getComputerChoice() {
+    /* get random number 0, 1, or 2 */
+    number = Math.floor(Math.random() * 3);
+
+    /* make constant for computer choice */
+    const computerChoice;
+
+    /* if random number is 0, choice is rock. 
+       if random number is 1, choice is paper. 
+       if random number si 2, choice is scissors */
+    if (number == 0) {
+        computerChoice = 'rock';
+    }
+    else if (number == 1) {
+        computerChoice = 'paper';
+    }
+    else {
+        computerChoice = 'scissors'
+    }
+
+    /* return computer choice */
+    return computerChoice
+}
+
